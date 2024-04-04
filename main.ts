c7302b6596cc25f353f0056a7bf9464a170ead80
alizaaatariq <165548868+alizaaatariq@users.noreply.github.com>
2024-04-04 06:17:52 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,5 @@
+//Question no 2
+//store a person name in a variable amd print a mesaage to them
+
+let personName: string = "Aliza";
+console.log(`hello ${personName} ,would you like to learn some python today  `);
