c2e603ac8c6c6bb73964b430e8325d3f8f902167
alizaaatariq <165548868+alizaaatariq@users.noreply.github.com>
2024-04-23 04:12:35 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-// let names= ['alisha','kanwal','nida']
-// let message : string= 'hows your studies going';
-// console.log("hello",names[0], message);
-// console.log("hello",names[1], message);
-// console.log("hello",names[2],message);
-var fnames = ['alisha', 'nisha ', 'aliza'];
-var message = "trust the lord and keep going ,never give up God will uplift you at the right time and give you success";
-console.log("hello", fnames[0], message);
-console.log("hello", fnames[1], message);
-console.log("hello", fnames[2], message);
+var transportation = ["porsch", "tesla", "range rover"];
+transportation.map(function (items) { return console.log("i would like to own ".concat(items)); });
--- main.ts
+++ main.ts
@@ -1,6 +1,2 @@
-
-let fnames= ['alisha', 'nisha ', 'aliza']
-let message: string= "trust the lord and keep going ,never give up God will uplift you at the right time and give you success"
-console.log("hello", fnames[0], message);
-console.log("hello",fnames[1],message);
-console.log("hello",fnames[2],message);
+let transportation: string[]= ["porsch","tesla","range rover"]
+transportation.map((items) => console.log(`i would like to own ${items}`));
