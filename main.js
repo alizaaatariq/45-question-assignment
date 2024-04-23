3d92bf6dc17d77cfbf333af179420c1001c630df
alizaaatariq <165548868+alizaaatariq@users.noreply.github.com>
2024-04-23 04:05:26 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-//addition 
-var add = 4 + 4;
-console.log("addition ".concat(add));
-//subtraction 
-var subt = 9 - 1;
-console.log("subtraction ".concat(subt));
-//multiplication
-var mult = 2 * 4;
-console.log("multiplication ".concat(mult));
-//division 
-var div = 16 / 2;
-console.log("division ".concat(div));
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
--- main.ts
+++ main.ts
@@ -1,15 +1,4 @@
-//addition 
-let add:number = 4+4;
-console.log(`addition ${add}`);
-
-//subtraction 
-let subt:number = 9-1;
-console.log(`subtraction ${subt}`);
-
-//multiplication
-let mult:number = 2*4;
-console.log(`multiplication ${mult}`);
-
-//division 
-let div:number = 16/2;
-console.log(`division ${div}`);
+console.log(5+3);
+console.log(11-3);
+console.log(4*2);
+console.log(16/2);
