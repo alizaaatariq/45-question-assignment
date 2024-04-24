ed8af072a7de485bb74b89d797338f899cb46e5c
alizaaatariq <165548868+alizaaatariq@users.noreply.github.com>
2024-04-24 05:18:31 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-var names = ["alisha", "nisha", "nazia"];
-names.map(function (items) { return console.log("hey ".concat(items, " you're invited for a dinner ")); });
+var guestList = ['alisha', 'nisha', 'nazia'];
+var notComing = guestList.splice(0, 1)[0];
+console.log("".concat(notComing, " dont make it to te dinner "));
+// push
+guestList.push("alaya");
+// print message
+guestList.forEach(function (guest) { console.log("dear ".concat(guest, " you're invited to the dinner")); });
--- main.ts
+++ main.ts
@@ -1,2 +1,7 @@
-let names:string[]=["alisha","nisha","nazia"]
-names.map((items) => console.log(`hey ${items} you're invited for a dinner `))
+let guestList: string[]=['alisha','nisha','nazia'];
+let notComing: string = guestList.splice(0,1)[0];
+console.log(`${notComing} dont make it to te dinner `);
+// push
+guestList.push("alaya");
+// print message
+guestList.forEach(guest => {console.log(`dear ${guest} you're invited to the dinner`)});
